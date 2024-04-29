9b9de434123937fc994f82d47b9c38a06747af5e
yeonsuBaek <soothbugger@gmail.com>
2024-04-29 16:51:32 +0900

    💄 Implement and style Segmented Component

--- src/lib/components/segmented/Segmented.tsx
+++ src/lib/components/segmented/Segmented.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import { SegmentedProps } from '.'
 
-const Segmented = ({}: SegmentedProps) => {
-  return <div>Segmented</div>
+const Segmented = ({
+  id,
+  options,
+  selectedOption,
+  onSelect,
+  size = 'medium',
+  width = 'full',
+  disabled = false,
+}: SegmentedProps) => {
+  return (
+    <div id={id} className={`ui-segmented ${size} ${width}`}>
+      {options.map((option) => (
+        <div className={`ui-segmented-option ${disabled ? 'disabled' : ''}`} key={option}>
+          <input
+            className="ui-segmented-option-radio"
+            type="radio"
+            id={`${id}-${option}`}
+            name={id}
+            value={option}
+            checked={selectedOption === option}
+            onChange={() => onSelect(option)}
+            disabled={disabled}
+          />
+          <label className="ui-segmented-option-label" htmlFor={`${id}-${option}`}>
+            {option}
+          </label>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Segmented
--- src/lib/components/segmented/index.tsx
+++ src/lib/components/segmented/index.tsx
@@ -1,5 +1,13 @@
 import Segmented from './Segmented'
 
-export interface SegmentedProps {}
+export interface SegmentedProps {
+  id: string
+  options: string[]
+  selectedOption: string
+  onSelect: (item: string) => void
+  size?: 'small' | 'medium' | 'large'
+  width?: 'full' | 'flex'
+  disabled?: boolean
+}
 
 export { Segmented }
