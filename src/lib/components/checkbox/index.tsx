b78faf6832e067e9563a920a2ac487f0e7e66b08
yeonsuBaek <soothbugger@gmail.com>
2024-04-30 09:49:57 +0900

    💄 Implement and style Checkbox component

--- src/lib/components/checkbox/index.tsx
+++ src/lib/components/checkbox/index.tsx
@@ -1,3 +1,12 @@
 import Checkbox from './Checkbox'
 
+export interface CheckboxProps {
+  id: string
+  options: string[]
+  selectedOptions: string[]
+  onSelect: (item: string) => void
+  wrap?: boolean
+  disabled?: boolean
+}
+
 export { Checkbox }
