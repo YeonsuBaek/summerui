55db79845a827706257952588877a807a13b369f
yeonsuBaek <soothbugger@gmail.com>
2024-05-20 15:59:31 +0900

    🚧 Add label, required props to PasswordTextfield

--- src/lib/components/textfield/PasswordTextField.tsx
+++ src/lib/components/textfield/PasswordTextField.tsx
@@ -2,12 +2,25 @@ import React, { useMemo, useState } from 'react'
 import { DefaultTextFieldProps } from '.'
 import { IconButton } from '../button'
 
-const PasswordTextField = ({ size = 'medium', disabled, id, value, onChange, placeholder }: DefaultTextFieldProps) => {
+const PasswordTextField = ({
+  id,
+  value,
+  onChange,
+  placeholder = '',
+  size = 'medium',
+  label = '',
+  required = false,
+  disabled = false,
+}: DefaultTextFieldProps) => {
   const [isShow, setIsShow] = useState(false)
   const iconSize = useMemo(() => (size === 'large' ? 'medium' : 'small'), [size])
 
   return (
-    <div className={`ui-textfield ${size} ${disabled ? 'disabled' : ''}`}>
+    <div
+      className={`ui-textfield ${size} ${placeholder ? 'placeholder' : ''} ${
+        label && size === 'large' ? 'label' : ''
+      } ${disabled ? 'disabled' : ''}`}
+    >
       <input
         className="ui-textfield-input"
         type={isShow ? 'text' : 'password'}
@@ -17,6 +30,11 @@ const PasswordTextField = ({ size = 'medium', disabled, id, value, onChange, pla
         placeholder={placeholder}
         disabled={disabled}
       />
+      {label && size === 'large' && (
+        <label htmlFor={id} className={`ui-textfield-label ${required ? 'required' : ''}`}>
+          {label}
+        </label>
+      )}
       <IconButton
         icon={isShow ? 'Eye' : 'EyeInvisible'}
         variant="secondary"
