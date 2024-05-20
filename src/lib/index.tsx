c190c2a88f144b20c7787c7eebf796fc0627b2dd
yeonsuBaek <soothbugger@gmail.com>
2024-05-20 16:11:09 +0900

    🚀 Deploy IconButton

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,7 +1,7 @@
 import './styles/index.scss'
 
 export { Icon } from './components/icon'
-export { Button } from './components/button'
+export { Button, IconButton } from './components/button'
 export { TextField, PasswordTextField } from './components/textfield'
 export { Portal } from './components/portal'
 export { Backdrop } from './components/backdrop'
