d363fce18135db95af0742c27a89feb09ffb5f17
yeonsuBaek <soothbugger@gmail.com>
2024-06-04 10:19:33 +0900

    ✅ Add CheckboxGroup Test

--- src/lib/components/checkbox/CheckboxGroup.tsx
+++ src/lib/components/checkbox/CheckboxGroup.tsx
@@ -16,28 +16,6 @@ const CheckboxGroup = ({ options, checkedOptions, onChange, wrap = false }: Chec
           )
         })}
     </div>
-    // <div id={id} className={`ui-checkbox ${wrap ? 'wrap' : ''} ${disabled ? 'disabled' : ''}`}>
-    //   {options.map((option) => (
-    //     <div className="ui-checkbox-option" key={option}>
-    //       <input
-    //         className="ui-checkbox-option-check"
-    //         type="checkbox"
-    //         id={`${id}-${option}`}
-    //         name={id}
-    //         value={option}
-    //         checked={selectedOptions.includes(option)}
-    //         onChange={() => onSelect(option)}
-    //         disabled={disabled}
-    //       />
-    //       <span className={`ui-checkbox-option-icon ${selectedOptions.includes(option) ? 'checked' : ''}`}>
-    //         {selectedOptions.includes(option) && <Icon icon="Check" />}
-    //       </span>
-    //       <label className="ui-checkbox-option-label" htmlFor={`${id}-${option}`}>
-    //         {option}
-    //       </label>
-    //     </div>
-    //   ))}
-    // </div>
   )
 }
 
