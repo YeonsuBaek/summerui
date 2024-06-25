e92709fd72aa3347f74eac7410f1060e458195dc
yeonsuBaek <soothbugger@gmail.com>
2024-06-26 07:50:04 +0900

    💄 Declare width of Card freely

--- src/lib/components/card/Card.stories.tsx
+++ src/lib/components/card/Card.stories.tsx
@@ -4,18 +4,15 @@ import { Card } from '.'
 const meta: Meta<typeof Card> = {
   title: 'Component/Card',
   component: Card,
-  argTypes: {
-    size: ['medium', 'large'],
-  },
+  argTypes: {},
 }
 
 export default meta
 
-const InteractionTemplate: ComponentStory<typeof Card> = (args) => <Card {...args} id="story-card-1"></Card>
+const InteractionTemplate: ComponentStory<typeof Card> = (args) => <Card {...args} id="story-card-1" width={200}></Card>
 
 export const Interactive = InteractionTemplate.bind({})
 Interactive.args = {
-  size: 'medium',
   title: '트로피컬 나이트',
   info: '조예은 (지은이)',
   image: 'https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9791160408331.jpg',
@@ -24,8 +21,8 @@ Interactive.args = {
 export const DefaultCard = () => {
   return (
     <div>
-      <Card id="story-card-2" title="Card 1" info="This is information of Card 1." image="" size="medium" />
-      <Card id="story-card-3" title="Card 1" info="This is information of Card 1." image="" size="large" />
+      <Card id="story-card-2" title="Card 1" info="This is information of Card 1." image="" width={150} />
+      <Card id="story-card-3" title="Card 1" info="This is information of Card 1." image="" width={250} />
     </div>
   )
 }
--- src/lib/components/card/Card.tsx
+++ src/lib/components/card/Card.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { CardProps } from '.'
 
-const Card = ({ id, title, info, image, size = 'medium', onClick }: CardProps) => {
+const Card = ({ id, title, info, image, onClick, width }: CardProps) => {
   return (
-    <figure id={id} className={`ui-card ${size}`} role="button" onClick={onClick}>
+    <figure id={id} className="ui-card" role="button" onClick={onClick} style={{ width: width || 'auto' }}>
       <img className={`ui-card-image ${image ? '' : 'blank'}`} src={image} alt={title} />
       <figcaption className="ui-card-figcaption">
         <span className="ui-card-figcaption-title">{title}</span>
