71aacce6e357e4ac45f940d7b7779263f59da4ea
yeonsuBaek <soothbugger@gmail.com>
2024-07-11 08:58:37 +0900

    ✨ Automate SVG to React component conversion and formatting

---
+++ src/assets/icon/generate-icon.sh
@@ -0,0 +1,5 @@
+cd svgs 
+npx @svgr/cli@5.5.0 --template ../utils/svgr-cli.template.js --config-file ../utils/.svgrrc *.svg --out-dir ../generated --ext tsx
+cd ../utils/ 
+node generate-export.js 
+cd ../ && prettier --write .
