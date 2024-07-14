6801e47f3605ee1e54b3337d072eacb2cac29b7b
yeonsuBaek <soothbugger@gmail.com>
2024-07-15 08:42:38 +0900

    ✨ Implement TextArea component

---
+++ src/components/textarea/TextArea.tsx
@@ -0,0 +1,78 @@
+import { ChangeEvent, useEffect, useMemo, useRef } from 'react'
+import { TextAreaProps } from './TextArea.types'
+
+export const TextArea = ({
+  id,
+  value,
+  onChange = () => {},
+  size = 'medium',
+  placeholder = '',
+  label = '',
+  maxLength,
+  rows = 3,
+  helperText = '',
+  error = false,
+  errorText = '',
+  autoFocus = false,
+  readOnly = false,
+  disabled = false,
+}: TextAreaProps) => {
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+
+  const valueLength = useMemo(() => value.length, [value])
+  const isMaxValueLength = useMemo(() => maxLength && valueLength > maxLength, [valueLength, maxLength])
+
+  const hasError = error || isMaxValueLength
+  const isDisabled = disabled || readOnly
+
+  const hasHelperText = error ? !errorText : helperText
+  const hasErrorText = errorText && error
+
+  useEffect(() => {
+    if (autoFocus && inputRef.current) {
+      const current = inputRef.current
+      current.focus()
+    }
+  }, [autoFocus])
+
+  return (
+    <div className="ui-textarea">
+      {label !== '' && (
+        <label className="ui-textarea-label" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <div
+        className={`ui-textarea-input ${size} ${readOnly ? 'read-only' : ''} ${disabled ? 'is-disabled' : ''} ${
+          hasError ? 'is-error' : ''
+        }`}
+      >
+        <textarea
+          id={id}
+          value={value}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => onChange(e.target.value)}
+          placeholder={placeholder}
+          rows={rows}
+          disabled={isDisabled}
+          ref={inputRef}
+          aria-describedby={hasErrorText ? `${id}-error` : helperText ? `${id}-helper` : undefined}
+        />
+      </div>
+      <div className="ui-textarea-info">
+        {(hasErrorText || hasHelperText) && (
+          <p
+            id={hasError ? `${id}-error` : `${id}-helper`}
+            className={`ui-textarea-msg ${hasErrorText ? 'error' : ''} `}
+          >
+            {hasErrorText ? errorText : helperText}
+          </p>
+        )}
+        {maxLength && (
+          <span>
+            <strong className={`value-length ${isMaxValueLength ? 'max' : ''}`}>{valueLength}</strong>/{maxLength}
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
