b3fe659706d1159e170433adb5084b0fafbae78c
yeonsuBaek <soothbugger@gmail.com>
2024-07-15 15:30:42 +0900

    📝 Adjust .theme-dark to TextArea storybook

--- src/components/textarea/TextArea.stories.tsx
+++ src/components/textarea/TextArea.stories.tsx
@@ -13,24 +13,44 @@ export const SizesOfTextArea = () => {
   const [val, setVal] = useState('')
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-      <TextArea
-        id="storybook-textarea-size-1"
-        value={val}
-        onChange={setVal}
-        size="small"
-        label="Small Size"
-        placeholder="Placeholder 플레이스홀더"
-      />
-      <TextArea
-        id="storybook-textarea-size-2"
-        value={val}
-        onChange={setVal}
-        size="medium"
-        label="Medium Size"
-        placeholder="Placeholder 플레이스홀더"
-      />
-    </div>
+    <>
+      <div style={{ padding: '16px', display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <TextArea
+          id="storybook-textarea-size-1"
+          value={val}
+          onChange={setVal}
+          size="small"
+          label="Small Size"
+          placeholder="Placeholder 플레이스홀더"
+        />
+        <TextArea
+          id="storybook-textarea-size-2"
+          value={val}
+          onChange={setVal}
+          size="medium"
+          label="Medium Size"
+          placeholder="Placeholder 플레이스홀더"
+        />
+      </div>
+      <div className="theme-dark" style={{ padding: '16px', display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <TextArea
+          id="storybook-textarea-size-1-dark"
+          value={val}
+          onChange={setVal}
+          size="small"
+          label="Small Size"
+          placeholder="Placeholder 플레이스홀더"
+        />
+        <TextArea
+          id="storybook-textarea-size-2-dark"
+          value={val}
+          onChange={setVal}
+          size="medium"
+          label="Medium Size"
+          placeholder="Placeholder 플레이스홀더"
+        />
+      </div>
+    </>
   )
 }
 
@@ -38,57 +58,115 @@ export const MaxLengthOfTextArea = () => {
   const [val, setVal] = useState('')
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-      <TextArea
-        id="storybook-textarea-length-1"
-        value={val}
-        onChange={setVal}
-        label="Has Max Length only"
-        placeholder="Placeholder 플레이스홀더"
-        maxLength={20}
-      />
-      <TextArea
-        id="storybook-textarea-size-2"
-        value={val}
-        onChange={setVal}
-        size="medium"
-        label="Has Max Length and Message"
-        placeholder="Placeholder 플레이스홀더"
-        helperText="20자 이하 작성해주세요."
-        maxLength={20}
-      />
-      <TextArea
-        id="storybook-textarea-size-3"
-        value="이럴 수가... 20자를 넘겨버렸다!!!"
-        size="medium"
-        label="Has Max Length and Message"
-        placeholder="Placeholder 플레이스홀더"
-        helperText="20자 이하 작성해주세요."
-        maxLength={20}
-      />
-    </div>
+    <>
+      <div style={{ padding: '16px', display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <TextArea
+          id="storybook-textarea-length-1"
+          value={val}
+          onChange={setVal}
+          label="Has Max Length only"
+          placeholder="Placeholder 플레이스홀더"
+          maxLength={20}
+        />
+        <TextArea
+          id="storybook-textarea-size-2"
+          value={val}
+          onChange={setVal}
+          size="medium"
+          label="Has Max Length and Message"
+          placeholder="Placeholder 플레이스홀더"
+          helperText="20자 이하 작성해주세요."
+          maxLength={20}
+        />
+        <TextArea
+          id="storybook-textarea-size-3"
+          value="이럴 수가... 20자를 넘겨버렸다!!!"
+          size="medium"
+          label="Has Max Length and Message"
+          placeholder="Placeholder 플레이스홀더"
+          helperText="20자 이하 작성해주세요."
+          maxLength={20}
+        />
+      </div>
+      <div className="theme-dark" style={{ padding: '16px', display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <TextArea
+          id="storybook-textarea-length-1-dark"
+          value={val}
+          onChange={setVal}
+          label="Has Max Length only"
+          placeholder="Placeholder 플레이스홀더"
+          maxLength={20}
+        />
+        <TextArea
+          id="storybook-textarea-length-2-dark"
+          value={val}
+          onChange={setVal}
+          size="medium"
+          label="Has Max Length and Message"
+          placeholder="Placeholder 플레이스홀더"
+          helperText="20자 이하 작성해주세요."
+          maxLength={20}
+        />
+        <TextArea
+          id="storybook-textarea-length-3-dark"
+          value="이럴 수가... 20자를 넘겨버렸다!!!"
+          size="medium"
+          label="Has Max Length and Message"
+          placeholder="Placeholder 플레이스홀더"
+          helperText="20자 이하 작성해주세요."
+          maxLength={20}
+        />
+      </div>
+    </>
   )
 }
 export const TextAreaGuide = () => {
   return (
-    <TextArea
-      id="storybook-textarea-guide-1"
-      value=""
-      label="후기"
-      placeholder="예) 즐겁게 관람했습니다."
-      helperText="영화 관람 후기를 10자 이상 작성해주세요."
-      maxLength={10}
-    />
+    <>
+      <div style={{ padding: '16px' }}>
+        <TextArea
+          id="storybook-textarea-guide-1"
+          value=""
+          label="후기"
+          placeholder="예) 즐겁게 관람했습니다."
+          helperText="영화 관람 후기를 10자 이상 작성해주세요."
+          maxLength={10}
+        />
+      </div>
+      <div className="theme-dark" style={{ padding: '16px' }}>
+        <TextArea
+          id="storybook-textarea-guide-1-dark"
+          value=""
+          label="후기"
+          placeholder="예) 즐겁게 관람했습니다."
+          helperText="영화 관람 후기를 10자 이상 작성해주세요."
+          maxLength={10}
+        />
+      </div>
+    </>
   )
 }
 export const TextAreaWithoutLabel = () => {
   return (
-    <TextArea
-      id="storybook-textarea-label-1"
-      value=""
-      placeholder="Placeholder 플레이스홀더"
-      helperText="label이 없는 입력창입니다."
-      aria-label="TextArea without label"
-    />
+    <>
+      <div style={{ padding: '16px' }}>
+        <TextArea
+          id="storybook-textarea-label-1"
+          value=""
+          placeholder="Placeholder 플레이스홀더"
+          helperText="label이 없는 입력창입니다."
+          aria-label="TextArea without label"
+        />
+      </div>
+      <div className="theme-dark" style={{ padding: '16px' }}>
+        <TextArea
+          id="storybook-textarea-label-1-dark"
+          value=""
+          placeholder="Placeholder 플레이스홀더"
+          helperText="label이 없는 입력창입니다."
+          aria-label="TextArea without label"
+        />
+      </div>
+    </>
   )
 }
