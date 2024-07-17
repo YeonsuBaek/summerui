062c77e79843e60c97938c09c27db4fb7762b322
yeonsuBaek <soothbugger@gmail.com>
2024-07-17 13:30:22 +0900

    ✨ Implement Radio component

---
+++ src/components/radio/Radio.tsx
@@ -0,0 +1,35 @@
+import { ChangeEvent } from 'react'
+import { RadioProps } from './Radio.types'
+import { CircleIcon } from '../../assets/icon'
+
+export const Radio = ({
+  id,
+  children,
+  name,
+  value,
+  size = 'medium',
+  checked = false,
+  onChange = () => {},
+  disabled = false,
+}: RadioProps) => {
+  return (
+    <div className={`ui-radio ${size}`}>
+      <span className="ui-radio-icon" role="radio" aria-checked={checked} aria-disabled={disabled}>
+        {checked && <CircleIcon size={12} />}
+      </span>
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.checked)}
+        disabled={disabled}
+        aria-labelledby={`${id}-label`}
+      />
+      <label className="ui-radio-label" htmlFor={id} id={`${id}-label`}>
+        {children}
+      </label>
+    </div>
+  )
+}
