0af82e930ce8ec6a44bb09b70951bfb23a1c2b4f
YeonsuBaek <soothbugger@gmail.com>
2024-07-20 09:43:31 +0900

    🔨 Add test:c script for testing with coverage reporting

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['<rootDir>/src/components/**/*.test.tsx'],
+}
