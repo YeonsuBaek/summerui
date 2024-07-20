21566e88cf0806903b7290f4d530e050a1efa906
YeonsuBaek <soothbugger@gmail.com>
2024-07-20 14:47:45 +0900

    ✨ Implement DatePicker component

---
+++ src/components/datepicker/DatePicker.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import { CalendarBlankFilled } from '../../assets/icon/generated/CalendarBlankFilled'
+import { Button } from '../button'
+import { TextField } from '../textfield'
+import { DatePickerProps } from './DatePicker.types'
+import Datetime from 'react-datetime'
+import 'react-datetime/css/react-datetime.css'
+import moment, { Moment } from 'moment'
+
+export const DatePicker = ({
+  id,
+  value,
+  onChange = () => {},
+  size = 'medium',
+  format = 'YYYY/MM/DD',
+  label = '',
+  helperText = '',
+  isError = false,
+  errorText = '',
+  autoFocus = false,
+  readOnly = false,
+  disabled = false,
+}: DatePickerProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const iconSize = size === 'small' ? 'small' : 'medium'
+
+  const handleChange = (selectedDate: string | Moment | null) => {
+    if (moment.isMoment(selectedDate)) {
+      onChange(selectedDate.format(format))
+    }
+  }
+
+  return (
+    <div className={`ui-datepicker ${size}`}>
+      <TextField
+        id={id}
+        value={value}
+        size={size}
+        placeholder={format}
+        label={label}
+        helperText={helperText}
+        isError={isError}
+        errorText={errorText}
+        autoFocus={autoFocus}
+        readOnly={readOnly}
+        disabled={disabled}
+        onChange={onChange}
+      >
+        <Button styleType="icon" size={iconSize} onClick={() => setIsOpen((prev) => !prev)}>
+          <CalendarBlankFilled />
+        </Button>
+      </TextField>
+      {isOpen && (
+        <Datetime
+          input={false}
+          timeFormat={false}
+          dateFormat={format}
+          value={moment(value, format)}
+          onChange={handleChange}
+        />
+      )}
+    </div>
+  )
+}
---
+++ src/components/datepicker/DatePicker.types.ts
@@ -0,0 +1,18 @@
+type DatePickerSize = 'small' | 'medium' | 'large'
+
+type DatePickerFormat = 'YYYYMMDD' | 'YYYY/MM/DD' | 'YYYY.MM.DD' | 'YYYY-MM-DD' | 'YYMMDD'
+
+export interface DatePickerProps {
+  id: string
+  value: string
+  onChange?: (val: string) => void
+  size?: DatePickerSize
+  format?: DatePickerFormat
+  label?: string
+  helperText?: string
+  isError?: boolean
+  errorText?: string
+  autoFocus?: boolean
+  readOnly?: boolean
+  disabled?: boolean
+}
